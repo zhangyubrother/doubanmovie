5ecfbd9f7a918dd979cc191a47cdebe92b3f83a2
master <feiyu9823@gmail.com>
2016-10-27 17:33:43 +0800

    添加依赖

---
+++ app/src/main/java/com/marks/asuper/doubanmovie/model/DoubanBean.java
@@ -0,0 +1,18 @@
+package com.marks.asuper.doubanmovie.model;
+
+import java.util.List;
+
+/**
+ * Created by Administrator on 2016/10/27.
+ */
+
+public class DoubanBean {
+
+    private int count;
+    private String title;
+    private List<Subjects> mSubjectses;
+
+    public class Subjects {
+
+    }
+}
