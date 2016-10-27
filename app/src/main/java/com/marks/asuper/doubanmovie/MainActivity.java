1358ba80c91681e96934aec93a6389f76e4736bc
Snail1995 <fenghongjing2016@163.com>
2016-10-27 17:06:29 +0800

    创建DoubanMovie

---
+++ app/src/main/java/com/marks/asuper/doubanmovie/MainActivity.java
@@ -0,0 +1,13 @@
+package com.marks.asuper.doubanmovie;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
